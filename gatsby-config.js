c9e25d98cfdd12617f3bec5dd46d21fa8fa51935
Alexander Kiefer <alexanderkiefer@Alexanders-MacBook-Pro.local>
2020-04-13 22:50:56 +0200

    Added gatsby plugins for netlify cms use

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,42 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-plugin-netlify-cms'
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/_posts/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1000,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Arvo`,
+            variants: [`400`, `700`]
+          }
+        ],
+      },
+    }
   ]
 }
