2f6b03c3d6c0c4c62d681b62acb8cc75d96e68e2
Alexander Kiefer <alexanderkiefer@Alexanders-MacBook-Pro.local>
2020-04-23 14:33:07 +0200

    Added heading to projects

--- src/pages/projekte.js
+++ src/pages/projekte.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import SiteWrapper from '../components/siteWrapper';
 import AllProjects from '../components/projects/allProjects';
+
+const H1 = styled.h1`
+    font-weight: 400;
+`
+const H2 = styled.h2`
+    font-weight: 400;
+`
 
 const Projekte = (props) => {
     let wasPage = false;
@@ -11,6 +19,12 @@ const Projekte = (props) => {
     }
     return (
         <SiteWrapper seoTitle="Alex Kiefer - Projekte" isPage wasPage={wasPage}>
+            <H1 className={ wasPage ? 'animated fadeInLeft' : 'animated fadeInLeft delay-1s' }>
+                Projekte
+            </H1>
+            <H2 className={ wasPage ? 'animated fadeInLeft' : 'animated fadeInLeft delay-1s' }>
+                Hier findest du eine Auswahl an Projekten, an denen ich in letzter Zeit gearbeitet habe.
+            </H2>
             <AllProjects wasPage={wasPage} />
         </SiteWrapper>
     );
